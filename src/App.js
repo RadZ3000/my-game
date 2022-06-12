dc03f5803367bc6ac3325361a30d28a7549329ad
RadZ3000 <herald.adowei@gmail.com>
2022-06-12 12:53:15 -0500

    Edited Attack Messages and Mechanics

--- src/App.js
+++ src/App.js
@@ -9,26 +9,44 @@ class GameApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      textDiv: "Fight!",
       player1Health: 500,
       player1Attack: ItemsObj.Sword,
       player2Health: 500,
-      player2Attack: ItemsObj.Spear
+      player2Attack: ItemsObj.Spear,
+      description: ""
     }
   }
 
   handleAttack = (event) => {
     this.setState({
-      player2Health: this.state.player2Health - Math.floor(Math.random() * (this.state.player1Attack + 1))
+      textDiv: "You try to strike..."
     });
+
+    setTimeout(() => {
+      this.setState({
+        player2Health: this.state.player2Health - Math.floor(Math.random() * (this.state.player1Attack + 1)),
+        textDiv: "You hit the opponent!"
+      })
+    }, 1000)
     document.getElementById("attack").disabled = true;
   }
 
+  handleItems = (event) => {
+
+  }
+
   handleEndTurn = (event) => {
+    this.setState({
+      textDiv: "Opponent's Turn..."
+    })
+
     setTimeout(() => {
       this.setState({
-        player1Health: this.state.player1Health - Math.floor(Math.random() * (this.state.player2Attack + 1))
+        player1Health: this.state.player1Health - Math.floor(Math.random() * (this.state.player2Attack + 1)),
+        textDiv: "You got hit!"
       })
-    }, 500)
+    }, 1000)
 
     document.getElementById("attack").disabled = false;
 
@@ -38,12 +56,14 @@ class GameApp extends React.Component {
     return (
       <div>
 
-        <h1>Fight!</h1>
+        <div id="text-div"><h1>{this.state.textDiv}</h1></div>
 
         <div id="player1">Player 1: {this.state.player1Health} HP</div>
         <div id="player2">Player 2: {this.state.player2Health} HP</div>
         <button id="attack" onClick={this.handleAttack}>Attack</button>
+        <button id="items" onClick={this.handleItems}>Items</button>
         <button id="end-turn" onClick={this.handleEndTurn}>End Turn</button>
+        <div id="description-paragraph">{this.state.description}</div>
       </div>
     )
   }
