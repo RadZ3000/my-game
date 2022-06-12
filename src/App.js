ae4d585b2d2330ca62a1d4ab89897e55e6054a85
RadZ3000 <herald.adowei@gmail.com>
2022-06-11 20:54:36 -0500

    App Backbones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ItemsObj from './Items';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+window.onbeforeunload = (e) => {
+  return "Are you sure you want to leave? Changes you made may not be saved!";
 }
 
-export default App;
+class GameApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player1Health: 500,
+      player1Attack: ItemsObj.Sword,
+      player2Health: 500,
+      player2Attack: ItemsObj.Spear
+    }
+  }
+
+  handleAttack = (event) => {
+    this.setState({
+      player2Health: this.state.player2Health - Math.floor(Math.random() * (this.state.player1Attack + 1))
+    });
+    document.getElementById("attack").disabled = true;
+  }
+
+  handleEndTurn = (event) => {
+    setTimeout(() => {
+      this.setState({
+        player1Health: this.state.player1Health - Math.floor(Math.random() * (this.state.player2Attack + 1))
+      })
+    }, 500)
+
+    document.getElementById("attack").disabled = false;
+
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h1>Fight!</h1>
+
+        <div id="player1">Player 1: {this.state.player1Health} HP</div>
+        <div id="player2">Player 2: {this.state.player2Health} HP</div>
+        <button id="attack" onClick={this.handleAttack}>Attack</button>
+        <button id="end-turn" onClick={this.handleEndTurn}>End Turn</button>
+      </div>
+    )
+  }
+}
+
+export default GameApp;
---
+++ src/Items.js
@@ -0,0 +1,7 @@
+let ItemsObj = {
+    Sword: 20,
+    Spear: 15,
+    Axe: 25
+}
+
+export default ItemsObj;
